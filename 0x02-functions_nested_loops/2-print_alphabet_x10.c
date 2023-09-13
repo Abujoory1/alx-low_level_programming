1494019be8f8d3e90abe054788af1fe8a3680bb6
Abujoory1 <mr.musamu@gmail.com>
2023-09-13 06:59:00 -0700

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - prints alphabet in lowercase 10x
+ * print_alphabet_x10 - prints alphabet in lowercase 10 times
+ *
  * Return: 0
  */
-
 void print_alphabet_x10(void)
 {
 	int i;
