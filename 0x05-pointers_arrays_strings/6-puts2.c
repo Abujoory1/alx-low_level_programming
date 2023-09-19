ea08a48d3d25319b9f93aae117f19d6d2d282331
Abujoory1 <mr.musamu@gmail.com>
2023-09-18 17:01:15 -0700

    6. Half the lies: This function prints every other character of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * puts2 - Print one char out of 2 of a string.
+ * @str: Type char pointer
+ * Return: Always 0.
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+} 
