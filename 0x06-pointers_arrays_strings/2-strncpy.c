01d5bee63e5f48c9cd497f01f3d7384d15976cf7
Abujoory1 <mr.musamu@gmail.com>
2023-09-21 17:15:27 -0700

    2. strncpy: copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,20 +2,32 @@
 #include <stdio.h>
 
 /**
- * _strcmp - Function that compares two strings.
- * @s1: type str
- * @s2: type str
+ * *_strncpy - Function that copies a string.
+ * @dest: string
+ * @src: string
+ * @n: number of chars to copy over
  * Return: Always 0.
  */
-int _strcmp(char *s1, char *s2)
+char *_strncpy(char *dest, char *src, int n)
 
 {
-	int a;
+	int a = 0;
+	int b = 0;
 
-	for (a = 0; s1[a] != '\0' && s2[a] != '\0'; a++)
+	while (a != n)
 	{
-		if (s1[a] != s2[a])
-		return (s1[a] - s2[a]);
+		dest[a] = src[b];
+
+		if (src[a] == '\0')
+		{
+			dest[b] = '\0';
+			break;
+		}
+		a++;
+		b++;
 	}
-	return (0);
+	while (b != n)
+		dest[b++] = '\0';
+
+	return (dest);
 }
