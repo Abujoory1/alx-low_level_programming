026d44f3ab81e0c7c2b5ff772fc9c7701e1a7de7
Abujoory1 <mr.musamu@gmail.com>
2023-10-20 17:51:01 -0700

    100. commit

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * before_main - Function executed before main
+ * Description: This function is before the main function
+ * Return: Void
+ */
+
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
